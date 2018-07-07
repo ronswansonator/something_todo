fda64a21869b387c86f74965a50a02a7d2d6f3ca
warddav16 <warddav16@gmail.com>
2018-07-07 09:27:43 -0700

    can move

---
+++ somethingtodo/Assets/Scripts/CustomCharacterController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class CustomCharacterController : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+
+    public float Speed = 10.0f;
+
+	void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate()
+    {
+        Vector2 dir = Vector2.zero;
+        dir.y += Input.GetAxis("Vertical");
+        dir.x += Input.GetAxis("Horizontal");
+        dir *= Speed * Time.fixedDeltaTime;
+
+        _rigidbody.MovePosition(_rigidbody.position + dir);
+	}
+}
